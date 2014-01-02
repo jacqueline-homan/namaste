e001a5e281ecfe4ed4ea57205e643015a898d1b0
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-01-02 03:46:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ankursapp::Application.config.secret_token = '5686bdb0d9a90f6b3de5f1e8679c0f3992d778b5700cf5bb75fbe15d38c2bd91e8bb402d77eb5de4d4fc9bb47583f049db3d7a4c2540dedef442d2ab2d2bb6dc'
